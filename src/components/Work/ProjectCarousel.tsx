80bd243a7fe9687e28e39890e467517de1f35ceb
Suan KC <kcsuan424@gmail.com>
2025-01-01 19:37:12 +0545

    responsive portfolio

--- src/components/Work/ProjectCarousel.tsx
+++ src/components/Work/ProjectCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { Project } from './types';
 import { ProjectDetails } from './ProjectDetails';
 import { ProjectNavigation } from './ProjectNavigation';
